d64caa49e61effb9831a564d2be595f2debfaf59
Suyash Kubade <suyashkubade@gmail.com>
2025-01-15 12:22:14 +0530

    New Message

--- js/hello.js
+++ js/hello.js
@@ -7,7 +7,7 @@ function HelloWorld(id) {
   greeting = document.createElement('h3');
   greeting.setAttribute('id', 'hello');
   greeting.style.visibility = "hidden";
-  greeting.innerHTML = "Hello Suyash Kubade";
+  greeting.innerHTML = "Hello Suyash! \n How Are You?";
 
   element.appendChild(greeting);
 
